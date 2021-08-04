e82093e07a7f54a51ec4f46e13219b0bff090a97
Kirill Danilychev <k3499@yandex.ru>
2021-08-04 13:02:05 +0300

    fix: фикс бага

--- app.js
+++ app.js
@@ -11,14 +11,7 @@ const auth = require('./middlewares/auth');
 const notRoutes = require('./routes/notRoutes');
 const allErrors = require('./errors/allErrors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-// Массив доменов, с которых разрешены кросс-доменные запросы
-const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'localhost:3000',
-  'https://mesto.k3499.nomoredomains.club',
-];
-
+const cors = require('cors');
 const app = express();
 
 const { PORT = 3000 } = process.env;
@@ -31,25 +24,18 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 app.use('/', express.json());
 
-app.use((req, res, next) => {
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers']; // сохраняем список заголовков исходного запроса
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы из любого источника
-    res.header('Access-Control-Allow-Origin', '*');
-  }
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-  }
-  return next();
-});
+const options = {
+  origin: [
+    'http://localhost:3000',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
+app.use('*', cors(options));
 
 app.use(helmet());
 app.use(cookieParser());
