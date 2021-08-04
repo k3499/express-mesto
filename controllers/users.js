1e0ea84a1ec69228712cafe9285fdb19edb702cf
Kirill Danilychev <k3499@yandex.ru>
2021-08-04 13:41:41 +0300

    fix: фикс бага

--- controllers/users.js
+++ controllers/users.js
@@ -93,13 +93,6 @@ const login = (req, res, next) => {
           { expiresIn: '7d' },
         );
         res.send({ token });
-        // return res
-        //   .cookie('jwt', token, {
-        //     maxAge: 3600000 * 24 * 7,
-        //     httpOnly: true,
-        //     sameSite: true,
-        //   })
-        //   .send({ message: 'Вы успешно авторизованны!' });
       });
     })
     .catch(next);
